d2ab95df3ddcd0d3f6d5f3bc2db5d13ced861bcd
mpsalunggg <putrasatria893@gmail.com>
2024-09-09 10:12:35 +0700

    add env file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyABokqZPZUCXKUMeNGAGa9DRFIdVGzuwRs',
-  authDomain: 'realtime-chat-999fa.firebaseapp.com',
-  projectId: 'realtime-chat-999fa',
-  storageBucket: 'realtime-chat-999fa.appspot.com',
-  messagingSenderId: '1063680542940',
-  appId: '1:1063680542940:web:30b507bf381b36c89acc19',
-  measurementId: 'G-PJX7WNWTZC',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
